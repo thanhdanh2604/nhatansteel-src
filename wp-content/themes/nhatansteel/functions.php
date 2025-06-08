56f136f9bcee7a5b473fe7d0c0cea7ab146dac7a
thanhdanh2604 <thanhdanh2604@gmail.com>
2025-06-09 01:28:10 +0700

    fix all customer change, add contact page, contact form 7

--- wp-content/themes/nhatansteel/functions.php
+++ wp-content/themes/nhatansteel/functions.php
@@ -1,10 +1,8 @@
 <?php
 
 function nhatan_enqueue_styles() {
-    // wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
-    wp_enqueue_style('custom-style', get_stylesheet_directory_uri() . '/assets/css/custom.css');
-    wp_enqueue_script('custom-js', get_stylesheet_directory_uri() . '/assets/js/custom.js', array('jquery'), '1.0', true);
+    wp_enqueue_style('custom-style', get_template_directory_uri() . '/assets/css/custom.css');
+    wp_enqueue_script('custom-js', get_template_directory_uri() . '/assets/js/custom.js', array('jquery'), '1.0', true);
 }
 add_action('wp_enqueue_scripts', 'nhatan_enqueue_styles');
 
